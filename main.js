0c3895f4a37d1d836db6939c71e2faeead919336
ArneC <christiansarne04@gmail.com>
2026-02-10 19:30:32 +0100

    Basic Setup
    Created index.html, styles.css and main.js. Added HTML Boilerplate to index.html and linked the html document to the css and js. Imported CSS-Reset into styles.css

---
+++ main.js
@@ -0,0 +1,15 @@
+const tictactoe = {
+    playerOneScore: 0,
+    playerTwoScore: 0,
+
+    getWinningPlayer() {
+        //Logic to get the Player with more Points
+    },
+    resetScore() {
+        //Logic to reset Player Points to 0
+    }
+}
+
+const gameboard = {
+    
+}
