8dd24f015b3a47bdbc3398c8b0679e15321a8bb6
ArneC <christiansarne04@gmail.com>
2026-02-10 20:47:56 +0100

    Updated main.js
    Working on implementing the functionality for a demo console TTT version

--- main.js
+++ main.js
@@ -1,15 +1,67 @@
-const tictactoe = {
-    playerOneScore: 0,
-    playerTwoScore: 0,
+function Gameboard() {
+    const rows = 3;
+    const columns = 3;
+    const board = [];
 
-    getWinningPlayer() {
-        //Logic to get the Player with more Points
-    },
-    resetScore() {
-        //Logic to reset Player Points to 0
+    for(let i = 0; i < rows; i++) {
+        board[i] = [];
+        for(let j = 0; j < columns; j++) {
+            board[i].push(Cell());
+        }
+    }
+
+    const getBoard = () => board;
+
+    return {getBoard}
+}
+
+function Cell() {
+    let state = "";
+
+    const addMarker = (player) => {
+        state = this.marker;
+    }
+
+    const getState = () => state;
+
+    return {
+        addMarker,
+        getState
+    };
+}
+
+function GameController (playerOneName, playerTwoName) {
+    const board = Gameboard();
+
+    const players = [
+        {
+            name: playerOneName,
+            marker: "X"
+        },
+        {
+            name: playerTwoName,
+            marker: "O"
+        }
+    ];
+
+    let activePlayer = players[0];
+
+    const switchPlayerTurn = () => {
+        activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    }
+    const getActivePlayer = () => activePlayer;
+
+    const printNewRound = () => {
+        console.log(`It's ${getActivePlayer().marker}'s turn`)
+    }
+
+    const checkWinner = () => {
+        
+    }
+
+
+    const playRound = () => {
+        //Logic to play a round
     }
 }
 
-const gameboard = {
-    
-}
